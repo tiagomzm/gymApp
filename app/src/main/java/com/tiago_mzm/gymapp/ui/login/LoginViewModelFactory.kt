d3c31f765824d37045965d1079cae14168444852
abedoyaf18 <60907572+abedoyaf18@users.noreply.github.com>
2023-08-02 17:55:03 -0500

    Login V1

---
+++ app/src/main/java/com/tiago_mzm/gymapp/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.tiago_mzm.gymapp.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.tiago_mzm.gymapp.data.LoginDataSource
+import com.tiago_mzm.gymapp.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
