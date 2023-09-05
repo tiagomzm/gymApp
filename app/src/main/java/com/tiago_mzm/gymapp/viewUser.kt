9e86276f6c297c62f798acf096e645b6bdda601e
Dalejam <dalisalmova@gmail.com>
2023-09-04 22:30:51 -0500

    Fragment pago

--- app/src/main/java/com/tiago_mzm/gymapp/viewUser.kt
+++ app/src/main/java/com/tiago_mzm/gymapp/viewUser.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class viewUser : AppCompatActivity() {
@@ -14,6 +15,8 @@ class viewUser : AppCompatActivity() {
         val usuario = bundle?.getString("usuario")
         val textoUsuario = findViewById<TextView>(R.id.textView18)
         textoUsuario.text = usuario
+
+
     }
 
     fun salirVistaUsuario(view: View){
