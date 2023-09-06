ef3111e0577b67219a2a65c1d3f60e6ab6275b47
Dalejam <dalisalmova@gmail.com>
2023-09-05 21:32:28 -0500

    Fragment actualizar datos - pagar

--- app/src/main/java/com/tiago_mzm/gymapp/viewUser.kt
+++ app/src/main/java/com/tiago_mzm/gymapp/viewUser.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
 class viewUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,27 @@ class viewUser : AppCompatActivity() {
         val textoUsuario = findViewById<TextView>(R.id.textView18)
         textoUsuario.text = usuario
 
+        val btnEditar = findViewById<Button>(R.id.actualizarDatos)
+        val btnPago = findViewById<Button>(R.id.button)
+
+
+        btnEditar.setOnClickListener {
+            val nuevo = edit()
+            val manager = supportFragmentManager
+            val transaction = manager.beginTransaction()
+            transaction.replace(R.id.fragmentContainerView,nuevo)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
+        btnPago.setOnClickListener {
+            val nuevo = FragmentPago()
+            val manager = supportFragmentManager
+            val transaction = manager.beginTransaction()
+            transaction.replace(R.id.fragmentContainerView,nuevo)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
 
     }
 
