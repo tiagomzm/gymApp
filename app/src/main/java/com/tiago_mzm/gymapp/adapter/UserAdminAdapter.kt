9453b36ddf3efe4afe5999be84097e3ed2099819
abedoyaf18 <60907572+abedoyaf18@users.noreply.github.com>
2023-09-05 21:26:34 -0500

    Filtro v2

--- app/src/main/java/com/tiago_mzm/gymapp/adapter/UserAdminAdapter.kt
+++ app/src/main/java/com/tiago_mzm/gymapp/adapter/UserAdminAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.tiago_mzm.gymapp.data.AdminUser
 import com.tiago_mzm.gymapp.R
 
-class UserAdminAdapter(private val userList:List<AdminUser>) : RecyclerView.Adapter<UserAdminViewHolder>() {
+class UserAdminAdapter(private var userList:List<AdminUser>) : RecyclerView.Adapter<UserAdminViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAdminViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return UserAdminViewHolder(layoutInflater.inflate(R.layout.item_adminuser, parent, false))
@@ -19,6 +19,10 @@ class UserAdminAdapter(private val userList:List<AdminUser>) : RecyclerView.Adap
         val item = userList[position]
         holder.render(item)
 
+    }
+    fun listtaActualizada(userList: List<AdminUser>){
+        this.userList = userList
+        notifyDataSetChanged()
     }
 
 }
--- app/src/main/java/com/tiago_mzm/gymapp/admin.kt
+++ app/src/main/java/com/tiago_mzm/gymapp/admin.kt
@@ -4,28 +4,41 @@ package com.tiago_mzm.gymapp
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tiago_mzm.gymapp.adapter.UserAdminAdapter
+import com.tiago_mzm.gymapp.databinding.ActivityAdminBinding
 
 
 class admin : AppCompatActivity() {
+    private lateinit var binding: ActivityAdminBinding
+    private lateinit var adapter:UserAdminAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin)
+        binding = ActivityAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         initRecyclerView()
         val bundle = intent.extras
         val usuario = bundle?.getString("usuario")
         Toast.makeText(this@admin,usuario , Toast.LENGTH_SHORT)
             .show()
-        val textoUsuario = findViewById<TextView>(R.id.textView16)
-        textoUsuario.text = "admin"
+        //val textoUsuario = findViewById<TextView>(R.id.textView16)
+        //textoUsuario.text = "admin"
        // val documento =  findViewById<TextView>(R.id.textView16)
        // documento.text = "890984746-7"
+
+        binding.editTextText7.addTextChangedListener { usuarioFilrado ->
+           val usauriosFiltrados =  AdminUserListProvider.userList.filter {
+                    usuario -> usuario.nombre.lowercase().contains(usuarioFilrado.toString().lowercase()) }
+            adapter.listtaActualizada(usauriosFiltrados)
+        }
     }
 
     fun salirVista(view: View){
@@ -37,5 +50,6 @@ class admin : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.reciclerview_usuarios)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = UserAdminAdapter(AdminUserListProvider.userList)
+
     }
 }
