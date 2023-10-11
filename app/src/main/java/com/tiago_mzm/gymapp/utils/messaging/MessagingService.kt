69766482693494c0921018742f3e5eadac728bc5
Alexander.bedoya <yuber04@hotamail.com>
2023-10-11 17:40:42 -0500

    Commit v2 funcionado

--- app/src/main/java/com/tiago_mzm/gymapp/utils/messaging/MessagingService.kt
+++ app/src/main/java/com/tiago_mzm/gymapp/utils/messaging/MessagingService.kt
@@ -1,6 +1,7 @@
 package com.tiago_mzm.gymapp.utils.messaging
 
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MessagingService: FirebaseMessagingService() {
 }
