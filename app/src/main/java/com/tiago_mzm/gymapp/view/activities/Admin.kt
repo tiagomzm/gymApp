725a949afcea610fd3e2c19f74c4f3da35ba254a
abedoyaf18 <60907572+abedoyaf18@users.noreply.github.com>
2023-11-08 00:19:56 -0500

    Cloud Firestore completo

--- app/src/main/java/com/tiago_mzm/gymapp/view/activities/Admin.kt
+++ app/src/main/java/com/tiago_mzm/gymapp/view/activities/Admin.kt
@@ -8,7 +8,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.tiago_mzm.gymapp.view.recyclers.AdminUserListProvider
 import com.tiago_mzm.gymapp.adapter.UserAdminAdapter
 import com.tiago_mzm.gymapp.model.entities.AdminUser
@@ -22,6 +24,7 @@ class admin : AppCompatActivity() {
     private var listaUsuariosTratados: MutableList<AdminUser> = AdminUserListProvider.userList.toMutableList()
 
     val db : FirebaseFirestore = FirebaseFirestore.getInstance()
+    val collectionRef = db.collection("soportes")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,11 +45,11 @@ class admin : AppCompatActivity() {
             adapter.listtaActualizada(usauriosFiltrados)
         }
 
-        binding.Promociones.setOnClickListener {
+        binding.btnConsulta.setOnClickListener {
             var datos = ""
-            db.collection("promociones").get()
+            db.collection("soportes").get()
                 .addOnSuccessListener { resultado -> for (promocion in resultado){
-                    datos += "${promocion.id}: ${promocion.data}\n"
+                    datos += "${promocion.id}: ${promocion.data} + \n"
                 }
                     binding.textView7.text = datos
             }
@@ -55,8 +58,45 @@ class admin : AppCompatActivity() {
                 }
         }
 
+        binding.btnayuda.setOnClickListener {
+            guardarDatos(db)
+        }
+
+        binding.btnborrar.setOnClickListener {
+            if (binding.TxVsugerencia.text.isNotBlank()) {
+                db.collection("soportes")
+                    .document("3")
+                    .delete()
+                    .addOnSuccessListener { _ ->
+                        Toast.makeText(this, "borrado", Toast.LENGTH_SHORT).show()
+                    }
+
+                    .addOnFailureListener { _ ->
+                        Toast.makeText(this, "fallo", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
 
     }
+
+    private fun guardarDatos(db: FirebaseFirestore) {
+        if (binding.TxVsugerencia.text.isNotBlank()) {
+            val dato = hashMapOf(
+                "soportes" to binding.TxVsugerencia.text.toString()
+            )
+            db.collection("soportes")
+                .document("3")
+                .set(dato)
+                .addOnSuccessListener { _ ->
+                    Toast.makeText(this, "exito", Toast.LENGTH_SHORT).show()
+                }
+
+                .addOnFailureListener { _ ->
+                    Toast.makeText(this, "fallo", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
 
     fun salirVista(view: View){
         val intent= Intent(this, LoginActivity ::class.java).apply {  }
