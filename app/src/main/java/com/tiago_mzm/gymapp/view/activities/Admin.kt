68f715a9aaf6535509120071bc680921dbe7ee62
abedoyaf18 <60907572+abedoyaf18@users.noreply.github.com>
2023-11-07 20:18:40 -0500

    Cloud Firestore

--- app/src/main/java/com/tiago_mzm/gymapp/view/activities/Admin.kt
+++ app/src/main/java/com/tiago_mzm/gymapp/view/activities/Admin.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
 import com.tiago_mzm.gymapp.view.recyclers.AdminUserListProvider
 import com.tiago_mzm.gymapp.adapter.UserAdminAdapter
 import com.tiago_mzm.gymapp.model.entities.AdminUser
@@ -15,9 +16,12 @@ import com.tiago_mzm.gymapp.databinding.ActivityAdminBinding
 
 
 class admin : AppCompatActivity() {
+
     private lateinit var binding: ActivityAdminBinding
     private lateinit var adapter: UserAdminAdapter
     private var listaUsuariosTratados: MutableList<AdminUser> = AdminUserListProvider.userList.toMutableList()
+
+    val db : FirebaseFirestore = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +40,19 @@ class admin : AppCompatActivity() {
             val usauriosFiltrados =  AdminUserListProvider.userList.filter {
                     usuario -> usuario.nombre.lowercase().contains(usuarioFilrado.toString().lowercase()) }
             adapter.listtaActualizada(usauriosFiltrados)
+        }
+
+        binding.Promociones.setOnClickListener {
+            var datos = ""
+            db.collection("promociones").get()
+                .addOnSuccessListener { resultado -> for (promocion in resultado){
+                    datos += "${promocion.id}: ${promocion.data}\n"
+                }
+                    binding.textView7.text = datos
+            }
+                .addOnFailureListener{exception ->
+                    binding.textView7.text = "No se ha podido conectar"
+                }
         }
 
 
